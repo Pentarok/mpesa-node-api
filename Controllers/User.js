73a49f87b59f824766684c79704d16f424cc7855
Pentarok <167968478+Pentarok@users.noreply.github.com>
2025-04-21 00:32:25 +0300

    Update User.js

--- Controllers/User.js
+++ Controllers/User.js
@@ -323,7 +323,7 @@ exports.FinalizeResetPassword= async (req,res)=>{
 exports.getAccessToken = async ()=> {
   const consumer_key = process.env.CONSUMER_KEY;
   const consumer_secret = process.env.CONSUMER_SECRET;
-    "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+  const url=  "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
   const auth =
     "Basic " +
     new Buffer.from(consumer_key + ":" + consumer_secret).toString("base64");
