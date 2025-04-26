40ac81e665df1549171d0889d8482f2c1ddf5c41
Pentarok <makpentarok@gmail.com>
2025-04-26 03:15:43 +0300

    cors issue

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const {db}= require('./db/db')
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://expense-app-client-two.vercel.app",
     methods:["GET","POST","DELETE"],
     credentials:true
 }))
